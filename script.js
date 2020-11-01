4c63eb7282412001ffc255139b1bf1f1d0184164
Torontow <mtorontow0@gmail.com>
2020-11-01 16:31:54 -0500

    Further Construction
    Where before I had just added the rows for the scheduler, here I have added the columns as well, including the proper classes to apply from the CSS file.

--- script.js
+++ script.js
@@ -3,23 +3,21 @@ $(document).ready(function(){
 var timesArray = [9,10,11,12,1,2,3,4,5];
 
 for (var i = 0; i < timesArray.length; i++) {
+        var hour = $("<div>").addClass("col-1").attr("id",[i]).text(timesArray[i] + ":00");
+        var task = $("<textarea>").addClass("description col").attr("id",[i]);
+        var save = $("<div>").addClass("saveBtn col-1").attr("id",[i]);
 
-        var row = $("<div>");
-        row.addClass("row hour");
-        row.attr("id",[i]);
-        row.text(timesArray[i] + ":00");
-        // var hour = $("<div>").addClass("col-1").attr("id",[i]);
-        // console.log(hour);
-        // var task = $("<textarea>").addClass("description col").attr("id",[i]);
-        // var save = $("<div>").addClass("saveBtn col-1").attr("id",[i]);
-
-
-
-
+        var row = $("<div>").addClass("row hour").attr("id",[i]);
+        row.append(hour).append(task).append(save);
+        
         $("#scheduler").append(row);
-        // $("#scheduler").append(hour).append(task).append(save);
 
 }
+
+
+
+
+
 
 });
 
