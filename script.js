b34417058920a90780e40d59f0d686d5a2a34faa
Torontow <mtorontow0@gmail.com>
2020-11-03 09:38:08 -0500

    CSS FIx and Storage
    Used parseInt to correct an error affecting text area colours.
    Added storage functionality to scheduler

--- script.js
+++ script.js
@@ -1,35 +1,60 @@
-$(document).ready(function(){
+$(document).ready(function () {
+        // moment.js date for top of page
+        var today = moment().format("dddd, MMMM Do YYYY");
 
-var today = moment().format("dddd, MMMM Do YYYY");
-var currentHour = moment().format("H");
-var timesArray = [9,10,11,12,1,2,3,4,5];
-$("#currentDay").text(today);
-for (var i = 0; i < timesArray.length; i++) {
-        var hour = $("<div>").addClass("col-1").attr("id",[i]).text(timesArray[i] + ":00");
-        var todo = $("<textarea>").addClass("description col").attr("time",(i+9));
-        var slotHour = todo.attr("time");
-        if (slotHour === currentHour) {
-                todo.addClass("present");
-        } else if (slotHour < currentHour) {
-                todo.addClass("past");
-        } else if (slotHour > currentHour) {
-                todo.addClass("future");
+        var currentHour = parseInt(moment().format("HH"));
+        var timesArray = [9, 10, 11, 12, 1, 2, 3, 4, 5];
+        var todo;
+        var save;
+        var hour;
+
+        // Puts today's day and date in the scheduler header
+        $("#currentDay").text(today);
+
+        // builds the scheduler layout
+        for (var i = 0; i < timesArray.length; i++) {
+
+                // local variables to build layout
+                hour = $("<div>").addClass("col-1").attr("id", [i]).text(timesArray[i] + ":00");
+                todo = $("<textarea>").addClass("description col").attr("time", (i + 9)).attr("id",[i]);
+
+                save = $("<button>").addClass("saveBtn col-1").attr("id", [i]);
+                // builds the rows onto scheduler
+                var row = $("<div>").addClass("row hour").attr("id", [i]);
+                row.append(hour).append(todo).append(save);
+                $("#scheduler").append(row);
+
+                // styles the text areas dependent on what time it is
+                var slotHour = todo.attr("time");
+                console.log(slotHour);
+                console.log("it is " + currentHour + " o'clock");
+                if (slotHour < currentHour) {
+                        todo.addClass("past");
+                } else if (slotHour > currentHour) {
+                        todo.addClass("future");
+                } else {
+                        todo.addClass("present");
+                }
+
         }
 
 
+        // render the schedule function
 
-        var save = $("<div>").addClass("saveBtn col-1").attr("id",[i]);
-        var row = $("<div>").addClass("row hour").attr("id",[i]);
-        row.append(hour).append(todo).append(save);
-        $("#scheduler").append(row);
-        
-}
+        function renderScheduler() {
+                $
+        }
 
+        // store items function
+        $(".saveBtn").on("click", function(event){
+                event.preventDefault();
+                var saveIndex = $(this).attr("id");
+                var scheduleInput = $(this).siblings("textarea").val();
+                localStorage.setItem(saveIndex,scheduleInput);
 
+        })
 
-
-
-
+        // end of document ready function
 });
 
 
